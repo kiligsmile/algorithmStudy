6f3000b6bd15db0d5b85df95abc60844e27d3d2b
kiligsmile <kiligsmile@163.com>
2023-06-24 22:04:48 +0800

    力扣

---
+++ src/main/java/com/smile/leetcode/jzOffer/l2390.java
@@ -0,0 +1,46 @@
+package com.smile.leetcode.jzOffer;
+
+import java.util.Stack;
+
+public class l2390 {
+    public String removeStars(String s) {
+        Stack<Character> stack=new Stack<Character>();
+        int len=s.length();
+        int num=len;
+        for(int i=0;i<len;i++){
+            if(s.charAt(i)!='*'){
+                stack.push(s.charAt(i));
+            }
+            else{
+                stack.pop();
+                num=num-2;
+            }
+        }
+        char[] ch=new char[num];
+        int index=0;
+        while(!stack.empty()){
+            ch[index]=stack.pop();
+            index++;
+        }
+        String s1 = String.valueOf(ch);
+        return new StringBuffer(s1).reverse().toString();
+    }
+
+    public String removeStars2(String s) {
+        Stack<Character> stack=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        char [] cs=s.toCharArray();
+        for(char ch:cs){
+            if(ch == '*'){
+                stack.pop();
+            }else{
+                stack.push(ch);
+            }
+        }
+        for(Character ch:stack){
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+}
