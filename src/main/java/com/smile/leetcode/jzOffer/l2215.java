d9786c0d0d8c53281f1a619cd06b635aa94ad34a
kiligsmile <kiligsmile@163.com>
2023-07-01 21:57:22 +0800

    力扣

---
+++ src/main/java/com/smile/leetcode/jzOffer/l2215.java
@@ -0,0 +1,28 @@
+package com.smile.leetcode.jzOffer;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class l2215 {
+    class Solution {
+        public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+            HashSet<Integer> set1 = new HashSet<>();
+            HashSet<Integer> set2 = new HashSet<>();
+            for (int n : nums2) {
+                set2.add(n);
+            }
+            for (int n : nums1) {
+                set1.add(n);
+            }
+            for (int n : nums2) {
+                set1.remove(n);
+            }
+            for (int n : nums1) {
+                set2.remove(n);
+            }
+
+            return Arrays.asList(List.copyOf(set1), List.copyOf(set2));
+        }
+    }
+}
