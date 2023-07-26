2d14ac7f95fbf424c17097c241fa1eedb8fcd4e7
kiligsmile <kiligsmile@163.com>
2023-07-26 22:30:19 +0800

    力扣

---
+++ src/main/java/com/smile/leetcode/jzOffer/l1137.java
@@ -0,0 +1,22 @@
+package com.smile.leetcode.jzOffer;
+
+public class l1137 {
+    class Solution {
+        public int tribonacci(int n) {
+            if (n == 0) {
+                return 0;
+            }
+            if (n <= 2) {
+                return 1;
+            }
+            int p = 0, q = 0, r = 1, s = 1;
+            for (int i = 3; i <= n; ++i) {
+                p = q;
+                q = r;
+                r = s;
+                s = p + q + r;
+            }
+            return s;
+        }
+    }
+}
