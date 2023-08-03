f4b634eb1a1c807f01d23b37a6d376431d859e9a
kiligsmile <kiligsmile@163.com>
2023-08-03 19:48:14 +0800

    力扣

---
+++ src/main/java/com/smile/leetcode/jzOffer/l1732.java
@@ -0,0 +1,23 @@
+package com.smile.leetcode.jzOffer;
+
+public class l1732 {
+    class Solution {
+        public int largestAltitude(int[] gain) {
+            int height=0;
+            int max=0;
+            int arr[]=new int [gain.length+1];
+            arr[0]=0;
+            for(int i=1;i<arr.length;i++){
+                arr[i]=gain[i-1];
+            }
+            for(int i=1;i<arr.length;i++){
+                int x=arr[i]+arr[i-1];
+                arr[i]=x;
+                if(x>max){
+                    max=x;
+                }
+            }
+            return max;
+        }
+    }
+}
--- src/main/java/com/smile/leetcode/jzOffer/ll1776.java
+++ src/main/java/com/smile/leetcode/jzOffer/ll1776.java
@@ -1,23 +1,23 @@
-package com.smile.leetcode.jzOffer;
-
-public class l1776 {
-    class Solution {
-        public String mergeAlternately(String word1, String word2) {
-            int len1=word1.length();
-        int len2=word2.length();
-        StringBuilder s =new StringBuilder();
-        int j=0,i=0;
-        while(j<len1||i<len2){
-            if(j<len1){
-                s.append(word1.charAt(j));
-                j++;
-            }
-            if(i<len2){
-                s.append(word2.charAt(i));
-                i++;
-            }
-        }
-        return s.toString();
-        }
-    }
-}
+//package com.smile.leetcode.jzOffer;
+//
+//public class l1776 {
+//    class Solution {
+//        public String mergeAlternately(String word1, String word2) {
+//            int len1=word1.length();
+//        int len2=word2.length();
+//        StringBuilder s =new StringBuilder();
+//        int j=0,i=0;
+//        while(j<len1||i<len2){
+//            if(j<len1){
+//                s.append(word1.charAt(j));
+//                j++;
+//            }
+//            if(i<len2){
+//                s.append(word2.charAt(i));
+//                i++;
+//            }
+//        }
+//        return s.toString();
+//        }
+//    }
+//}
