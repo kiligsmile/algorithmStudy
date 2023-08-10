2b9b1bb90420ae3d5970d4e0e3b97c9ad352a5ab
kiligsmile <kiligsmile@163.com>
2023-08-10 20:01:25 +0800

    力扣

---
+++ src/main/java/com/smile/leetcode/l875.java
@@ -0,0 +1,34 @@
+package com.smile.leetcode;
+
+public class l875 {
+    class Solution {
+        public int minEatingSpeed(int[] piles, int h) {
+            int low = 1;
+            int high = 0;
+            for (int pile : piles) {
+                high = Math.max(high, pile);
+            }
+            int k = high;
+            while (low < high) {
+                int speed = (high - low) / 2 + low;
+                long time = getTime(piles, speed);
+                if (time <= h) {
+                    k = speed;
+                    high = speed;
+                } else {
+                    low = speed + 1;
+                }
+            }
+            return k;
+        }
+
+        public long getTime(int[] piles, int speed) {
+            long time = 0;
+            for (int pile : piles) {
+                int curTime = (pile + speed - 1) / speed;
+                time += curTime;
+            }
+            return time;
+        }
+    }
+}
