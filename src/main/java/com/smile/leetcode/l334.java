f64a403aa42790828dc2b66bcd052f9d59e4068c
kiligsmile <kiligsmile@163.com>
2023-08-12 21:43:51 +0800

    力扣

--- src/main/java/com/smile/leetcode/l334.java
+++ src/main/java/com/smile/leetcode/l334.java
@@ -21,4 +21,29 @@ public class l334 {
             return false;
         }
     }
+
+    class Solution2 {
+        public boolean increasingTriplet(int[] nums) {
+            int n=nums.length;
+            if(n<3){
+                return false;
+            }
+            int[] leftMin=new int[n];
+            leftMin[0]=nums[0];
+            for(int i=1;i<n;i++){
+                leftMin[i]=Math.min(leftMin[i-1],nums[i]);
+            }
+            int[] rightMax=new int[n];
+            rightMax[n-1]=nums[n-1];
+            for(int i=n-2;i>=0;i--){
+                rightMax[i]=Math.max(rightMax[i+1],nums[i]);
+            }
+            for(int i=1;i<n-1;i++){
+                if(nums[i]>leftMin[i-1]&&nums[i]<rightMax[i+1]){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
